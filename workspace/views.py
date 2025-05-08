85b0c48a88eb30d5bd3e55bcd406e25b0c750823
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-08 18:50:44 +0200

    feat: implement empty views for tasks and tags

--- workspace/views.py
+++ workspace/views.py
@@ -10,3 +10,45 @@ class TasksListView(generic.ListView):
     template_name = "workspace/tasks-list.html"
     context_object_name = "tasks"
 
+
+class TasksListCreateView(generic.CreateView):
+    pass
+
+
+class TaskListDetailView(generic.DetailView):
+    pass
+
+
+class TasksListUpdateView(generic.UpdateView):
+    pass
+
+
+class TasksListDeleteView(generic.DeleteView):
+    pass
+
+
+class TaskIsDoneView(generic.View):
+    pass
+
+
+#--------------------------TAGS------------------
+
+class TagsListView(generic.ListView):
+    pass
+
+
+class TagsListCreate(generic.CreateView):
+    pass
+
+
+class TagsListDetailView(generic.DetailView):
+    pass
+
+
+class TagsListUpdateView(generic.UpdateView):
+    pass
+
+
+class TagsListDeleteView(generic.DeleteView):
+    pass
+
