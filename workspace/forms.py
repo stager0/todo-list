68dd3f1b2cc994ef24af18b491fc20b3e98becb7
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-08 18:58:03 +0200

    feat: implemented forms.py with empty Task, Tag - creation and update forms

---
+++ workspace/forms.py
@@ -0,0 +1,16 @@
+from django.forms import forms
+
+
+class TaskCreationForm(forms.Form):
+    pass
+
+class TaskUpdateForm(forms.Form):
+    pass
+
+#---------------------------------------------Tags
+
+class TagCreationForm(forms.Form):
+    pass
+
+class TagUpdateForm(forms.Form):
+    pass
