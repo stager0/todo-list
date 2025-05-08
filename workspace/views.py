244eb34387c2f41cbae381e94c36429c1a13ab68
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-08 19:44:52 +0200

    feat: filled in views: TasksListView, TasksListCreateView, TaskListDetailView, TasksListUpdateView, TasksListDeleteView, TagsListView, TagsListCreate, TagsListDetailView, TagsListUpdateView, TagsListDeleteView

--- workspace/views.py
+++ workspace/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 
-from workspace.models import Task
+from workspace.forms import TagCreationForm, TagUpdateForm, TaskCreationForm, TaskUpdateForm
+from workspace.models import Task, Tag
 
 
 # Create your views here.
@@ -10,45 +11,84 @@ class TasksListView(generic.ListView):
     template_name = "workspace/tasks-list.html"
     context_object_name = "tasks"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.order_by("status")
+
 
 class TasksListCreateView(generic.CreateView):
-    pass
+    model = Task
+    template_name = "workspace/task-create.html"
+    form_class = TaskCreationForm
 
 
 class TaskListDetailView(generic.DetailView):
-    pass
+    model = Task
+    template_name = "workspace/task-detail.html"
+
+    def get_queryset(self):
+        return Task.objects.filter(pk=self.request.GET.get("pk"))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["task"] = self.get_object()
+        return context
 
 
 class TasksListUpdateView(generic.UpdateView):
-    pass
+    model = Task
+    template_name = "workspace/task-update.html"
+    form_class = TaskUpdateForm
 
 
 class TasksListDeleteView(generic.DeleteView):
-    pass
+    model = Task
+    template_name = "workspace/task-delete.html"
 
 
 class TaskIsDoneView(generic.View):
-    pass
+    def save(self):
+        pass
 
 
 #--------------------------TAGS------------------
 
 class TagsListView(generic.ListView):
-    pass
+    model = Tag
+    template_name = "workspace/tags-list.html"
+    context_object_name = "tags"
+
+    def get_queryset(self):
+        return Tag.objects.order_by("name")
 
 
 class TagsListCreate(generic.CreateView):
-    pass
+    model = Tag
+    template_name = "workspace/tag-create.html"
+    form_class = TagCreationForm
 
 
 class TagsListDetailView(generic.DetailView):
-    pass
+    model = Tag
+    template_name = "workspace/tag-detail.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        return Tag.objects.filter(pk=self.request.GET.get("pk"))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = self.get_object()
+        return context
 
 
 class TagsListUpdateView(generic.UpdateView):
-    pass
+    model = Tag
+    template_name = "workspace/tag-update.html"
+    form_class = TagUpdateForm
 
 
 class TagsListDeleteView(generic.DeleteView):
-    pass
+    model = Tag
+    template_name = "workspace/tag-delete.html"
 
