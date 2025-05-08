20ac0e7e6b392ccffde194bc42d640bdb9e200a1
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-08 23:32:59 +0200

    fix: remove field name from models

--- workspace/models.py
+++ workspace/models.py
@@ -6,11 +6,13 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=125)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
-    name = models.CharField(max_length=125, null=True, blank=True)
     content = models.CharField(max_length=210, null=True, blank=True)
     deadline = models.DateField(null=True, blank=True)
     created_at = models.DateField(auto_now_add=True)
-    status = models.BooleanField()
+    status = models.BooleanField(default=False)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
